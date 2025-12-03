5693d7c871ae8d55ef1e53e1ec995146270be160
Atharv Roxx <billionareatharv@gmail.com>
2025-12-03 14:26:30 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+import yt_dlp
+
+app = Flask(__name__)
+
+@app.route("/api/download")
+def download():
+    url = request.args.get("url")
+    if not url:
+        return jsonify({"error": "URL missing"}), 400
+
+    try:
+        ydl_opts = {"quiet": True, "skip_download": True}
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=False)
+
+        return jsonify({
+            "status": "ok",
+            "title": info.get("title"),
+            "thumbnail": info.get("thumbnail"),
+            "download_url": info["url"]
+        })
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+@app.route("/")
+def home():
+    return "Railway Video Downloader API Running"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
